80ebc4b3d8fe266e35e1a182dcf0041e11527afe
Nalzurin <alexnormandi@ukr.net>
2025-09-12 20:45:19 +0300

    Implement transporter event

---
+++ Source/HarmonyPatches/Patch_TravellingTransporters_Arrived_Event.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace EOTR.HarmonyPatches
+{
+    [HarmonyPatch]
+    public static class Patch_TravellingTransporters_Arrived_Event
+    {
+        public static MethodInfo TargetMethod()
+        {
+            return AccessTools.Method(typeof(TravellingTransporters), "Arrived");
+        }
+        public static bool Prefix(TravellingTransporters __instance, List<ActiveTransporterInfo> ___transporters, PlanetTile ___initialTile)
+        {
+            Log.Message("RAAAH");
+            Log.Message(___transporters.Count());
+            if (__instance.PodsHaveAnyFreeColonist)
+            {
+                Pawn p = __instance.Pawns.Where(p => p.IsColonist && p.HostFaction == null).RandomElement();
+                if (p != null)
+                {
+                    if (Rand.Chance(EchoesOfTheRim_Mod.Settings.chanceForInterruptionPerTile))
+                    {
+                        Helper.TrySetupTransporterEvent(__instance, ___transporters, ___initialTile, p);
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
--- Source/Helper.cs
+++ Source/Helper.cs
@@ -1,11 +1,17 @@
-﻿using RimWorld;
+﻿using LudeonTK;
+using RimWorld;
 using RimWorld.Planet;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using Unity.Collections;
+using UnityEngine;
 using Verse;
+using static System.Net.WebRequestMethods;
 
 namespace EOTR
 {
@@ -27,10 +33,8 @@ namespace EOTR
             else
             {
                 Find.WorldObjects.Add(site);
-                SendLetter(caravan, site);
+                SendLetterCaravan(caravan, site);
             }
- 
-            Log.Message("HIII");
         }
         public static SitePartDef GetRandomSite(PlanetLayer layer)
         {
@@ -42,9 +46,9 @@ namespace EOTR
                         .RandomElementByWeight(x => EchoesOfTheRim_Mod.Settings.structureDataActual[x].weight)
                         return def;*/
         }
-        public static void SendLetter(Caravan caravan, Site site)
+        public static void SendLetterCaravan(Caravan caravan, Site site)
         {
-            DiaNode startNode = new DiaNode("EOTR_CaravanSite_LetterText".Translate(site.def.LabelCap));
+            DiaNode startNode = new DiaNode("EOTR_CaravanSite_LetterText".Translate(site.LabelCap));
             DiaOption enterNow = new DiaOption("EOTR_CaravanSite_LetterEnterNow".Translate());
             enterNow.resolveTree = true;
             enterNow.action = () =>
@@ -59,6 +63,104 @@ namespace EOTR
             startNode.options.Add(enterNow);
             startNode.options.Add(markForLater);
             Find.WindowStack.Add(new Dialog_NodeTree(startNode));
+        }
+
+        public static void TrySetupTransporterEvent(TravellingTransporters travellingTransporters, List<ActiveTransporterInfo> transporters, PlanetTile initialTile, Pawn p)
+        {
+
+            Log.Message("Part 1");
+            PlanetTile eventTile = GetEventTile(initialTile, travellingTransporters.destinationTile);
+            Log.Message("Part 2");
+
+            SitePartDef def = GetRandomSite(eventTile.Layer);
+            Site site = SiteMaker.TryMakeSite([def], eventTile);
+            site.GetComponent<TimeoutComp>().StartTimeout(60000 * EchoesOfTheRim_Mod.Settings.despawnTimer);
+            if (site == null)
+            {
+                Log.Error("Could not find any valid faction for this site.");
+            }
+            else
+            {
+                Find.WorldObjects.Add(site);
+                CameraJumper.TryJump(site);
+                SendLetterTransporter(p, site);
+            }
+        }
+        private static void SendLetterTransporter(Pawn p, Site site)
+        {
+            DiaNode startNode = new DiaNode("EOTR_TransporterSite_LetterText".Translate(p.LabelCap, site.LabelCap));
+            DiaOption markForLater = new DiaOption("EOTR_CaravanSite_LetterMarkForLater".Translate());
+            markForLater.resolveTree = true;
+            startNode.options.Add(markForLater);
+            Find.WindowStack.Add(new Dialog_NodeTree(startNode));
+        }
+
+        public static PlanetTile GetEventTile(PlanetTile initialTile, PlanetTile destTile)
+        {
+            List<PlanetTile> pathTiles = GetTilesBetween2Tiles(initialTile, destTile);
+            List<PlanetTile> compatTiles = pathTiles.Distinct().Where(t => t.Valid && !Find.World.Impassable(t) && Find.WorldObjects.SiteAt(t) == null).ToList();
+            Log.Message(compatTiles.Count());
+            List<PlanetTile> possibleChoices = [.. compatTiles];
+            foreach (var t in compatTiles)
+            {
+                List<PlanetTile> neighbors = [];
+                Find.WorldGrid.GetTileNeighbors(t, neighbors);
+                possibleChoices.AddRange(neighbors.Where(t => t.Valid && !Find.World.Impassable(t) && Find.WorldObjects.SiteAt(t) == null && !possibleChoices.Contains(t)));
+            }
+            return possibleChoices.RandomElement();
+        }
+        public static List<PlanetTile> GetTilesBetween2Tiles(PlanetTile startTile, PlanetTile destTile)
+        {
+            if (!startTile.Valid)
+            {
+                Log.Error($"Tried to FindPath with invalid start tile {startTile}");
+                return null;
+            }
+            if (!destTile.Valid)
+            {
+                Log.Error($"Tried to FindPath with invalid dest tile {destTile}");
+                return null;
+            }
+            if (startTile.Layer != destTile.Layer)
+            {
+                Log.Error($"Tried to FindPath to a different layer {startTile} -> {destTile}");
+                return null;
+            }
+            PlanetTile tile = startTile;
+            World world = Find.World;
+            WorldGrid grid = world.grid;
+            Vector3 normalized = grid.GetTileCenter(destTile).normalized;
+            float[] array = world.pathGrid.layerMovementDifficulty[startTile.Layer];
+            List<PlanetTile> path = [tile];
+            int iterations = 0;
+            Log.Message("Starting loop");
+            while (tile != destTile || iterations < 3000)
+            {
+                Log.Message(iterations);
+                List<PlanetTile> neighbors = [];
+                tile.Layer.GetTileNeighbors(tile, neighbors);
+                PlanetTile closestNeighbor = neighbors.First();
+                float closestDist = grid.ApproxDistanceInTiles(GenMath.SphericalDistance(grid.GetTileCenter(closestNeighbor).normalized, normalized));
+                Log.Message(closestNeighbor);
+                for (int i = 1; i < neighbors.Count(); i++)
+                {
+                    Log.Message("Neighbor:");
+                    Log.Message(i);
+                    PlanetTile planetTile = neighbors[i];
+                    Log.Message(planetTile);
+                    Vector3 tileCenter = grid.GetTileCenter(planetTile);
+                    float potential = grid.ApproxDistanceInTiles(GenMath.SphericalDistance(tileCenter.normalized, normalized));
+                    if (potential < closestDist)
+                    {
+                        closestNeighbor = planetTile;
+                        closestDist = potential;
+                    }
+                }
+                path.Add(closestNeighbor);
+                tile = closestNeighbor;
+                iterations++;
+            }
+            return path;
         }
     }
 }
